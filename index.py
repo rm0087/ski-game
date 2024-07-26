2f54baa12f1caf518053b730f87d71187595d19f
Elizabeth <itsalaz@icloud.com>
2024-07-26 08:12:12 -0400

    worked on importing background .mov file using cv2, still having issues with window. Make sure to run the command pip install opencv-python.

--- index.py
+++ index.py
@@ -1,16 +1,38 @@
 import pygame
+from sys import exit
+import cv2 
+
+cap = cv2.VideoCapture('background/SKI_BACKGROUND.mov')
 
 pygame.init()
-if pygame.get_init():
-    print("pygame initialized successfully!")
-else:
-    raise Exception("pygame could not be initialized")
-
 keys = pygame.key.get_pressed()
-
 background = [1, 1, 2, 2, 2, 1]
 screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Upheaval.ttf', 30)
+
+
 for i in range(6):
     screen.blit(background[i], (i*10,0))
 playerpos = 3
 screen.blit(8, (playerpos*10,0))
+
+ 
+
+text_surface = test_font.render('X Ski Games', False, 'Blue')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            cap.release()
+            cv2.destroyAllWindows()
+            pygame.quit()
+            exit()
+
+
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    pygame.display.update()
+    clock.tick(60)
